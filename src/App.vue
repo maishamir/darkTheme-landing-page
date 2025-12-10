b0738ba6b0b3685a058b73147d694af6ad5c9a7e
Maisha Mir <maisha.mir2014@gmail.com>
2025-12-10 12:20:24 -0800

    signup form layout complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Button from './components/Button/Button.vue';
 import NavBar from './components/NavBar/NavBar.vue';
+import SignupForm from './components/SignupForm/SignupForm.vue';
 import TestimonyCard from './components/TestimonyCard/TestimonyCard.vue';
 import profileImg from "@/assets/images/profile-1.jpg";
 </script>
@@ -12,6 +13,8 @@ import profileImg from "@/assets/images/profile-1.jpg";
     <Button button-type="link" button-text="See how Fylo works" />
 
     <TestimonyCard card-copy="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine." :user-img="profileImg" user-name="Satish Patel" user-role="Founder & CEO" user-company="Huddle" />
+
+    <SignupForm />
   </main>
 </template>
 
---
+++ src/components/SignupForm/SignupForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Button from '../Button/Button.vue';
+
+</script>
+<template>
+    <div class="signupForm">
+        <h1 class="signupForm__title">
+            Get early access today
+        </h1>
+        <p class="signupForm__copy">
+            It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.
+        </p>
+
+        <form action="" class="signupForm__form">
+            <label for="email" hidden>Enter your email</label>
+            <input type="email" placeholder="email@example.com" name="email" id="email">
+            <Button button-type="button" button-text="Get Started For Free" />
+        </form>
+    </div>
+</template>
+<style lang="scss" scoped>
+</style>
