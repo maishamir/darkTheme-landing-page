8e12820775964551659d3b8c09a2f70c690cabc6
Maisha Mir <maisha.mir2014@gmail.com>
2025-12-10 12:05:01 -0800

    testimonial card layout complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Button from './components/Button/Button.vue';
 import NavBar from './components/NavBar/NavBar.vue';
+import TestimonyCard from './components/TestimonyCard/TestimonyCard.vue';
+import profileImg from "@/assets/images/profile-1.jpg";
 </script>
 
 <template>
@@ -8,6 +10,8 @@ import NavBar from './components/NavBar/NavBar.vue';
     <NavBar />
     <Button button-type="button" button-text="Get Started" />
     <Button button-type="link" button-text="See how Fylo works" />
+
+    <TestimonyCard card-copy="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine." :user-img="profileImg" user-name="Satish Patel" user-role="Founder & CEO" user-company="Huddle" />
   </main>
 </template>
 
---
+++ src/components/TestimonyCard/TestimonyCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+    defineProps(["cardCopy", "userImg", "userName", "userRole", "userCompany"])
+</script>
+
+<template> 
+    <div class="card">
+        <div class="card__body">
+            <p class="card__copy">
+                {{ cardCopy }}
+            </p>
+        </div>
+
+        <footer class="card__footer">
+            <img :src="userImg" :alt="userName" class="card__img">
+
+            <div class="card__userDeets">
+                <p class="card__username">
+                    {{ userName }}
+                </p>
+                <small class="card__role-company">
+                    {{ userRole }}, {{ userCompany }}
+                </small>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
