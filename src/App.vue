5dd49d682b22261efdf092bf8723efc01a6aea1d
Maisha Mir <maisha.mir2014@gmail.com>
2025-12-10 11:52:39 -0800

    button component rendered conditionally

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
-  import NavBar from './components/NavBar/NavBar.vue';
+import Button from './components/Button/Button.vue';
+import NavBar from './components/NavBar/NavBar.vue';
 </script>
 
 <template>
   <main class="app">
     <NavBar />
+    <Button button-type="button" button-text="Get Started" />
+    <Button button-type="link" button-text="See how Fylo works" />
   </main>
 </template>
 
 <style lang="scss" scoped>
-  .app {
-    background-color: $navy950;
-  }
+.app {
+  background-color: $navy950;
+}
 </style>
--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,11 +1,20 @@
 <script setup>
-    defineProps(['buttonText', 'buttonType'])
+defineProps(['buttonText', 'buttonType'])
+import arrowIcon from "@/assets/images/icon-arrow.svg";
+
 </script>
 
 <template>
-    <button class="button">
-        
-    </button>
+    <div class="button">
+        <button v-if="buttonType === 'button'" class="button--button">
+            {{ buttonText }}
+        </button>
+
+        <a href="#" v-else-if="buttonType === 'link'">
+            <p class="button--link">{{ buttonText }}</p>
+            <img :src="arrowIcon" alt="">
+        </a>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
